c59da5d437b0819be167bc401619dafdb59a9dfa
Tim Steinbach <tim@nequissimus.com>
2016-09-11 20:51:03 +0000

    Add Quill, MySQL, PostgreSQL

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,6 +12,11 @@ object Dependencies {
     val doobie = "org.tpolecat" %% "doobie-core-cats" % Versions.doobie
     val monix = "io.monix" %% "monix" % Versions.monix
     val monixCats = "io.monix" %% "monix-cats" % Versions.monix
+    val mysql = "mysql" % "mysql-connector-java" % Versions.mysql
+    val postgres = "org.postgresql" % "postgresql" % Versions.postgres
+    val quillCassandra = "io.getquill" %% "quill-cassandra" % Versions.quill
+    val quillJdbc = "io.getquill" %% "quill-jdbc" % Versions.quill
+    val quillPostgresAsync = "io.getquill" %% "quill-async-postgres" % Versions.quill
     val refined = "eu.timepit" %% "refined" % Versions.refined
     val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test"
     val shapeless = "com.chuusai" %% "shapeless" % Versions.shapeless
@@ -23,6 +28,9 @@ object Versions {
     val circe = "0.5.1"
     val doobie = "0.3.1-SNAPSHOT"
     val monix = "2.0.1"
+    val mysql = "5.1.38"
+    val postgres = "9.4.1208"
+    val quill = "0.10.1-SNAPSHOT"
     val refined = "0.5.0"
     val scala = "2.11.8"
     val scalaTest = "3.0.0"
